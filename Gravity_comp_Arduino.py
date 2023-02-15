800ea272c06cba214e85fcfe04abccd2cef36c87
panizhastam <sedighi1@ualberta.ca>
2023-02-14 18:43:40 -0700

    done

--- Gravity_comp_Arduino.py
+++ Gravity_comp_Arduino.py
@@ -1,14 +1,10 @@
 import sys
 import time
-import pickle
-import math
 import torch
-import numpy as np
-
 from telemetrix import telemetrix
+from torch import nn
+from math import pi
 
-import warnings
-# warnings.filterwarnings("ignore")
 
 """
 Monitor a digital input pin
@@ -52,8 +48,6 @@ PRESSURE_PIN_SHAA = 8
 DRIVER_PIN1_SHAA = 52
 DRIVER_PIN2_SHAA = 53
 
-
-
 ################################################################################################################################################
 # contribution of Ismail
 # best code ever
@@ -63,12 +57,14 @@ DRIVER_PIN2_SHAA = 53
 prev_pin = {'Elbow': [0,1], 
             'ShFE': [0,1],
             'ShAA': [0,1]}
-count ={'Elbow': 0, 
+count = {'Elbow': 0, 
             'ShFE': 0,
             'ShAA': 0}
-out = {'Elbow': 0, 
+joint_dir = {'Elbow': 0, 
             'ShFE': 0,
             'ShAA': 0}
+
+
 
 def compute_angle(pin_number,pin_value,joint):
     if joint == 'Elbow':        
@@ -76,32 +72,37 @@ def compute_angle(pin_number,pin_value,joint):
             if prev_pin[joint][0] != prev_pin[joint][1]:
                 if prev_pin[joint][1] != pin_value:
                     count[joint] += 1
+                    joint_dir[joint] = 0
                 else:
                     count[joint] -= 1
+                    joint_dir[joint] = 1
     if joint == 'ShFE':        
         if pin_number == DATA_PIN_ShFE:
             if prev_pin[joint][0] != prev_pin[joint][1]:
                 if prev_pin[joint][1] == pin_value:
                     count[joint] += 1
+                    joint_dir[joint] = 0
                 else:
                     count[joint] -= 1
+                    joint_dir[joint] = 1
     if joint == 'ShAA':        
         if pin_number == DATA_PIN_ShAA:
             if prev_pin[joint][0] != prev_pin[joint][1]:
                 if prev_pin[joint][1] == pin_value:
                     count[joint] += 1
+                    joint_dir[joint] = 0
                 else:
-                    count[joint] -= 1    
+                    count[joint] -= 1
+                    joint_dir[joint] = 1
     s =""
     for x in count:
-        s+= str(x) + ': '+ str(count[x]*360/1024) + '  ' # in (Deg)
-        out[x] = count[x]*(360/1024)*(math.pi/180) # in (rad)
+        s+= str(x) + ': '+ str(count[x]*360/1024) + "   " + 'dir: ' + str(joint_dir[x]) + "   "
         
     s+="\n"
-    # print(s)   
-    # print(out)
+    # print(s)
     prev_pin[joint][0] = pin_value
-    # return out
+    
+    #return [count['Elbow'], count['ShFE'], count['ShAA']]*360/1024
 
 def the_callback_elbow(data):
     """
@@ -114,6 +115,7 @@ def the_callback_elbow(data):
     date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data[CB_TIME]))
     # print(f'Pin Mode: {data[CB_PIN_MODE]} Pin: {data[CB_PIN]} Value: {data[CB_VALUE]} Time Stamp: {date}')
     compute_angle(data[CB_PIN],data[CB_VALUE],'Elbow')
+
 
 def the_callback_shfe(data):
     """
@@ -139,8 +141,64 @@ def the_callback_shaa(data):
     # print(f'Pin Mode: {data[CB_PIN_MODE]} Pin: {data[CB_PIN]} Value: {data[CB_VALUE]} Time Stamp: {date}')
     compute_angle(data[CB_PIN],data[CB_VALUE],'ShAA')
 
-# def Elbow_read():
+class Network(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(Network, self).__init__()
+
+        self.layer1 = nn.Linear(input_size, 64)
+        self.layer2 = nn.Linear(64, 32)
+        self.layer3 = nn.Linear(32, 16)
+        self.layer4 = nn.Linear(16, output_size)
+
+    def forward(self, x):
+        x = nn.functional.relu(self.layer1(x))
+        x = nn.functional.relu(self.layer2(x))
+        x = nn.functional.relu(self.layer3(x))
+        x = self.layer4(x)
+        return x
+    
+def predict(arr):
+    model = Network(6, 3)
+    path = "trainedmodels/MLP1.pth"
+    model.load_state_dict(torch.load(path))
+    model.eval()    
+    input = torch.tensor(arr)
+    predicted_output = model(input)
+    predicted_output.cpu().detach().numpy()
+    el = int(predicted_output[0][0])
+    fe = int(predicted_output[0][1]+30)
+    aa = int(predicted_output[0][2]+40)
+    print(str(el)+"   "+str(fe)+"   "+str(aa))
+    torque_control(el,fe,aa)
+
+    
+    
+def gravity_compensate():
+    arr = [[count['Elbow']*2*pi/1024, count['ShFE']*2*pi/1024, count['ShAA']*2*pi/1024, joint_dir['Elbow'], joint_dir['ShFE'], joint_dir['ShAA']]]
+    arr = torch.tensor(arr, dtype=torch.float32)
+    model = Network(6, 3)
+    path = "trainedmodels/MLP1.pth"
+    model.load_state_dict(torch.load(path))
+    model.eval()    
+    input = torch.tensor(arr)
+    predicted_output = model(input)
+    predicted_output.cpu().detach().numpy()
+    # el = int(predicted_output[0][0])
+    # fe = int(predicted_output[0][1]+50)
+    # aa = int(predicted_output[0][2]+70)
+    el = int(count['Elbow']*2.8**360/1024)
+    fe = int(count['ShFE']*2.8**360/1024)
+    aa = int(count['ShAA']*2.8**360/1024)
+    print(str(el)+"   "+str(fe)+"   "+str(aa))
+    torque_control(el,fe,aa)
+
+    # predict(arr)
+
+
+
 board = telemetrix.Telemetrix(arduino_wait=2)
+
+
 # setting Encoder input PINs
 board.set_pin_mode_digital_input(DATA_PIN_Elbow, the_callback_elbow)
 board.set_pin_mode_digital_input(STATE_PIN_Elbow, the_callback_elbow)
@@ -157,52 +215,115 @@ board.set_pin_mode_digital_input(STATE_PIN_ShAA, the_callback_shaa)
 
 
 
-# Setting Torque output PINs
-board.set_pin_mode_analog_output(PRESSURE_PIN_ELBOW)
-board.set_pin_mode_digital_output(DRIVER_PIN1_ELBOW)
-board.set_pin_mode_digital_output(DRIVER_PIN2_ELBOW)
 
-board.set_pin_mode_analog_output(PRESSURE_PIN_SHFE)
-board.set_pin_mode_digital_output(DRIVER_PIN1_SHFE)
-board.set_pin_mode_digital_output(DRIVER_PIN2_SHFE)
+def torque_control(el,fe,aa):
 
-board.set_pin_mode_analog_output(PRESSURE_PIN_SHAA)
-board.set_pin_mode_digital_output(DRIVER_PIN1_SHAA)
-board.set_pin_mode_digital_output(DRIVER_PIN2_SHAA)
+        # Setting Torque output PINs
+        board.set_pin_mode_digital_output(DRIVER_PIN1_ELBOW)
+        board.set_pin_mode_digital_output(DRIVER_PIN2_ELBOW)
+        # Setting PIN values
+        board.digital_write(DRIVER_PIN1_ELBOW, 0)
+        board.digital_write(DRIVER_PIN2_ELBOW, 1)
+        board.set_pin_mode_analog_output(PRESSURE_PIN_ELBOW)
+        board.analog_write(PRESSURE_PIN_ELBOW, el)
+        
+        board.set_pin_mode_digital_output(DRIVER_PIN1_SHFE)
+        board.set_pin_mode_digital_output(DRIVER_PIN2_SHFE)
+        board.digital_write(DRIVER_PIN1_SHFE, 0)
+        board.digital_write(DRIVER_PIN2_SHFE, 1)
+        board.set_pin_mode_analog_output(PRESSURE_PIN_SHFE)
+        board.analog_write(PRESSURE_PIN_SHFE, fe)
+        
+        board.set_pin_mode_digital_output(DRIVER_PIN1_SHAA)
+        board.set_pin_mode_digital_output(DRIVER_PIN2_SHAA)
+        board.digital_write(DRIVER_PIN1_SHAA, 0)
+        board.digital_write(DRIVER_PIN2_SHAA, 1)
+        board.set_pin_mode_analog_output(PRESSURE_PIN_SHAA)
+        board.analog_write(PRESSURE_PIN_SHAA, aa)
+        
+        # print("end")
+        
 
-
-# Setting PIN values
-board.digital_write(DRIVER_PIN1_ELBOW, 0)
-board.digital_write(DRIVER_PIN2_ELBOW, 1)
-
-board.digital_write(DRIVER_PIN1_SHFE, 0)
-board.digital_write(DRIVER_PIN2_SHFE, 1)
-
-board.digital_write(DRIVER_PIN1_SHAA, 0)
-board.digital_write(DRIVER_PIN2_SHAA, 1)
-
-
-# path = "trainedmodels/MLP1.sav"
-# loaded_model = pickle.load(open(path, 'rb'))
-
+##########################################     MAIN     #########################################################
 
 print('Enter Control-C to quit.')
 
-
+# back to rest position
 try:
     while True:
-        # x = out.values()
-        # x = list(x)
-        # x = np.array(x)
-        # x = x.reshape(1,3)
-        # teout = torch.Tensor(x)
-        # result = loaded_model.predict(teout)
-        # print(result)
-        board.analog_write(PRESSURE_PIN_ELBOW, 100)
-        time.sleep(.00001)
+        time.sleep(.001)
+        # gravity_compensate()
+        # torque_control(0,0,0)
+        el = int(abs(count['Elbow']*2*360/1024))
+        fe = int(abs(count['ShFE']*3*360/1024))
+        aa = int(abs(count['ShAA']*3*360/1024))
+        print(str(el)+"   "+str(fe)+"   "+str(aa))
+        torque_control(el,fe,aa)
 except KeyboardInterrupt:
     board.shutdown()
     sys.exit(0)
+
+
+# The max output torque for elbow is 200 for shfe is 256 and for shaa is 230
+# # All joints moving together
+# try:
+#     while True:
+#         print('Going up...')
+#         for i in range(256):
+#             torque_control('Elbow',i)
+#             torque_control('ShFE',i)
+#             torque_control('ShAA',i)
+#             time.sleep(1)  # controlling the frequency
+#             print(i)
+#             print(int(count['Elbow']*360/1024))
+#             if int(count['Elbow']*360/1024) == 30:
+#                 for j in range(i, -1, -1):
+#                     torque_control('Elbow',i)
+#                     time.sleep(1)  # controlling the frequency                
+#             if int(count['ShFE']*360/1024) == 20:
+#                 for j in range(i, -1, -1):
+#                     torque_control('ShFE',i)
+#                     time.sleep(1)  # controlling the frequency 
+#             if int(count['ShAA']*360/1024) == 20:
+#                 for j in range(i, -1, -1):
+#                     torque_control('ShAA',i)
+#                     time.sleep(1)  # controlling the frequency  
+# except KeyboardInterrupt:
+#     board.shutdown()
+#     sys.exit(0)
+
+# All joints moving together
+# try:
+#     print('Going up...')
+#     for i in range(200):
+#         torque_control('ShFE',i)
+#         time.sleep(2)  # controlling the frequency
+#         torque_control('ShAA',i)
+#         torque_control('Elbow',i)
+#         print(i)
+#     for i in range(200,-1,-1):
+#         torque_control('ShFE',i)
+#         time.sleep(2)  # controlling the frequency
+#         torque_control('ShAA',i)
+#         torque_control('Elbow',i)
+#         print(i)
+# except KeyboardInterrupt:
+#     board.shutdown()
+#     sys.exit(0)
+# try:
+#     while True:
+#         # x = out.values()
+#         # x = list(x)
+#         # x = np.array(x)
+#         # x = x.reshape(1,3)
+#         # teout = torch.Tensor(x)
+#         # result = loaded_model.predict(teout)
+#         # print(result)
+#         board.analog_write(PRESSURE_PIN_ELBOW, 100)
+#         time.sleep(.00001)
+# except KeyboardInterrupt:
+#     board.shutdown()
+#     sys.exit(0)
 
 
 
